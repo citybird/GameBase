3db5b1e0912a5e8fb981d4e5288a8457b8072f55
citybird <citybird.cn@gmail.com>
2014-10-31 17:58:02 +0800

    import Unity Test Tools

---
+++ Assets/Scripts/Config/Config.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Config : ScriptableObject
+{
+    /// <summary> 目标的资源平台.  </summary>
+    /// <remarks> </remarks>
+    public string targetPlatform;
+}
